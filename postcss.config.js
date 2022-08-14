3a74055207ec96cd8b01500e7f693d42da015fd7
Oscar Gerardo Trejo Rivera <ogerardtr_98@hotmail.com>
2022-08-14 18:58:10 -0500

    Mario con tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const tailwindConfig = require("./tailwind.config");
+
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        // '@tailwindcss/jit': {},
+        autoprefixer: {},
+    }
+}
